0074a3175c3eadc5e718fe8b031fca8f2fbe7b79
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-01-05 21:16:45 +0900

    controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/store'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
