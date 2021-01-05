d2b0b070071a9677ba7fc9549acf19719e7389fb
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-01-05 22:02:45 +0900

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(task_params)
-    redirect_to tasks_path
+      @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   private
